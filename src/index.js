cd9d687d3bce565fba7594f07eac5d02353997f8
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-07-01 20:19:45 +0530

    partha's initial commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import JobPortal from './JobPortal';
-import Login from './Login';
+// import Login from './Login';
+import Login from './login-page'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <JobPortal/>
+    <Login />
   </React.StrictMode>
 );
 
---
+++ src/login-page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Login() {
+    return (
+        <main className="container">
+            <div className="flex justify-start">
+                <div className="w-[40%] h-full"></div>
+                <div className="w-[60%] h-full bg-black"></div>
+            </div>
+        </main>
+    );
+}
+
+
+export default Login;
